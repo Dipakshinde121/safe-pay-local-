cf394fff8fcccd75dbcacca1e2a583f36936e05d
Dipak Shinde <dipakshinde121@users.noreply.github.com>
2026-07-12 22:28:15 +0530

    Initial commit: Add day1.js

---
+++ day1.js
@@ -0,0 +1,8 @@
+// Welcome to SafePay JavaScript Basics!
+// This is your day1.js file. We will use this to practice coding.
+
+// Example: Creating a constant variable for our app name
+const appName = "SafePay";
+
+// Example: Creating a variable for a payment amount (this can change)
+let paymentAmount = 500;
